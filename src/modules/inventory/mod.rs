3f69158100217099488eca881edf181b3d3e5731
Maruff <bmaruff@gmail.com>
2025-03-31 17:44:18 +0530

    structure for accounting, sales, purchase, invoice, and inventory

---
+++ src/modules/inventory/mod.rs
@@ -0,0 +1,5 @@
+// src/modules/base/mod.rs
+
+pub mod handlers;  // Declare `handlers` module
+pub mod services;  // Declare `services` module
+pub mod dto;  // Declare `dto` module
--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -1,3 +1,8 @@
 // src/modules/mod.rs
 
 pub mod base;  // Declare the `base` module inside `modules`
+pub mod accounting;  // Declare the `accounting` module inside `modules`
+pub mod inventory;  // Declare the `inventory` module inside `modules`
+pub mod sales;  // Declare the `sales` module inside `modules`
+pub mod purchase;  // Declare the `purchase` module inside `modules`
+pub mod invoice;  // Declare the `invoice` module inside `modules`
